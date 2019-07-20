b7889d530014b12fa1609aef2fbd07bbce2bd433
Abhinav Risal <abhinavrisal99@gmail.com>
2019-07-20 18:57:43 +0545

    Added conifg.php to gitignore

--- includes/config.php
+++ includes/config.php
@@ -3,7 +3,7 @@
 
     $timezone = date_default_timezone_set("Asia/Kathmandu");
 
-    $con = mysqli_connect("localhost","root","","");
+    $con = mysqli_connect("localhost","root","password","clonify");
 
     if(mysqli_connect_errno()){
         echo "Failed to connect: " . mysqli_connect_errno();
