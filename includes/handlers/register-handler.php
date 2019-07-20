78d9013b5e0d2b6248363dde1b4423717784a185
Abhinav Risal <abhinavrisal99@gmail.com>
2019-07-20 12:56:53 +0545

    Adding validations for inputs

--- includes/handlers/register-handler.php
+++ includes/handlers/register-handler.php
@@ -18,20 +18,43 @@
         return $inputText;
     }
 
+    function validateUsername($un) {
+        
+    }
+
+
+    function validateFirstName($fn) {
+        
+    }
+
+
+    function validateLastName($ln) {
+        
+    }
+
+    function validateEmails($em, $em2) {
+        
+    }
+
+
+    function validatePasswords($pw, $pw2) {
+        
+    }
+
     if(isset($_POST['registerButton'])) {
         $username = sanitizeFormUsername($_POST['username']);
-        
         $firstName = sanitizeFormString($_POST['firstName']);
-        
         $lastName = sanitizeFormString($_POST['lastName']);
-        
         $email = sanitizeFormString($_POST['email']);
-        
         $email2 = sanitizeFormString($_POST['email2']);
-        
         $password = sanitizeFormPassword($_POST['password']);
-        
         $password2 = sanitizeFormPassword($_POST['password2']);
+
+        validateUsername($username);
+        validateFirstName($firstName);
+        validateLastName($lastName);
+        validateEmails($email,$email2);
+        validatePasswords($password,$password2);
     }
     
 ?>
