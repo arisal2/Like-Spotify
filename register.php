39f67e53da0825a18d4a14eacd7b39420a153e57
Abhinav Risal <abhinavrisal99@gmail.com>
2019-07-20 11:33:10 +0545

    Initial commit

---
+++ register.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Welcome to Clonify</title>
+</head>
+<body>
+    <div id="inputContainer">
+        <form id="loginForm" action="register.php" method="POST">\
+            <h2>Login to your account</h2>
+        </form>
+    </div>
+</body>
+</html>
