a020280050b2692c7df8667be9e8cb8f8cdd1833
Abhinav Risal <abhinavrisal99@gmail.com>
2019-07-21 13:08:13 +0545

    Adding config alias for DB connetion info

---
+++ includes/config-alias.php
@@ -0,0 +1,15 @@
+<?php 
+
+// ob_start();
+// session_start();
+
+// $timezone = date_default_timezone_set("Asia/kathmandu");
+
+// $con = mysqli_connect("localhost","","","");
+
+// if(mysqli_connect_errno())
+// {
+//     echo "Failed to connect: " . mysqli_connect_errno();
+// }
+
+?>
