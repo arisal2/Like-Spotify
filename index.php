62393a83582f72afe3f36a8e4dd574cc5f870c8b
Abhinav Risal <abhinavrisal99@gmail.com>
2019-07-23 14:57:27 +0545

    Added progress bar divs and style elements

--- index.php
+++ index.php
@@ -16,10 +16,13 @@ else {
 </head>
 <body>
     <div id="nowPlayingBarContainer">
+
         <div id="nowPlayingBar">
+
             <div id="nowPlayingLeft">
 
             </div>
+
             <div id="nowPlayingCenter">
                 <div class="content playerControls">
                     <div class="buttons">
@@ -42,12 +45,34 @@ else {
                             <img src="assets/images/icons/repeat.png" alt="Repeat">
                         </button>
                     </div>
+
+                    <div class="playbackBar">
+                        <span class="progressTime current">0.00</span>
+
+                        <div class="progressBar">
+
+                            <div class="progressBarBg">
+
+                                <div class="progress">
+
+                                </div>
+                                
+                            </div>
+                            
+                            
+                        </div>
+
+                        <span class="progressTime remaining">0.00</span>
+                    </div>
                 </div>
             </div>
+
             <div id="nowPlayingRight">
 
             </div>
+
         </div>
+        
     </div>
 </body>
 </html>
