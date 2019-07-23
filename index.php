2002555bffaf851ce507d903dae1d0c216b8337f
Abhinav Risal <abhinavrisal99@gmail.com>
2019-07-23 19:44:19 +0545

    Added starting nav items

--- index.php
+++ index.php
@@ -25,8 +25,30 @@ $path = "assets/images/icons/";
             <div id="navBarContainer">
                 <nav class="navBar">
                     <a href="index.php" class="logo">
-                        <img src="<?php echo $path ?>music.png" alt="">
+                        <img src="<?php echo $path ?>music.png" alt="Music">
                     </a>
+
+                    <div class="group">
+
+                        <div class="navItem">
+                            <a href="search.php" class="navItemLink">Search</a>
+                        </div>
+
+                    </div>
+                    
+                    <div class="group">
+                        <div class="navItem">
+                            <a href="search.php" class="navItemLink">Browse</a>
+                        </div>
+                        <div class="navItem">
+                            <a href="search.php" class="navItemLink">Your Music</a>
+                        </div>
+                        <div class="navItem">
+                            <a href="search.php" class="navItemLink">Abhinav Risal</a>
+                        </div>
+
+                    </div>
+
                 </nav>
             </div>
 
