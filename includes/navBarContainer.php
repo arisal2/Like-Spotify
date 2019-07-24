fba2b710c9a9f30e4c1c3ea806a441389e4e47e2
Abhinav Risal <abhinavrisal99@gmail.com>
2019-07-24 20:16:29 +0545

    Added new file - navBarContainer inside include

---
+++ includes/navBarContainer.php
@@ -0,0 +1,31 @@
+<div id="navBarContainer">
+                <nav class="navBar">
+                    <a href="index.php" class="logo">
+                        <img src="<?php echo $path ?>music.png" alt="Music">
+                    </a>
+
+                    <div class="group">
+
+                        <div class="navItem">
+                            <a href="search.php" class="navItemLink">Search
+                                <img src="<?php echo $path ?>search.png" alt="Search" class="icon">
+                            </a>
+                        </div>
+
+                    </div>
+                    
+                    <div class="group">
+                        <div class="navItem">
+                            <a href="browse.php" class="navItemLink">Browse</a>
+                        </div>
+                        <div class="navItem">
+                            <a href="yourmusic.php" class="navItemLink">Your Music</a>
+                        </div>
+                        <div class="navItem">
+                            <a href="profile.php" class="navItemLink">Abhinav Risal</a>
+                        </div>
+
+                    </div>
+
+                </nav>
+            </div>
--- index.php
+++ index.php
@@ -22,38 +22,7 @@ $path = "assets/images/icons/";
 
         <div id="topContainer">
 
-            <div id="navBarContainer">
-                <nav class="navBar">
-                    <a href="index.php" class="logo">
-                        <img src="<?php echo $path ?>music.png" alt="Music">
-                    </a>
-
-                    <div class="group">
-
-                        <div class="navItem">
-                            <a href="search.php" class="navItemLink">Search
-                                <img src="<?php echo $path ?>search.png" alt="Search" class="icon">
-                            </a>
-                        </div>
-
-                    </div>
-                    
-                    <div class="group">
-                        <div class="navItem">
-                            <a href="browse.php" class="navItemLink">Browse</a>
-                        </div>
-                        <div class="navItem">
-                            <a href="yourmusic.php" class="navItemLink">Your Music</a>
-                        </div>
-                        <div class="navItem">
-                            <a href="profile.php" class="navItemLink">Abhinav Risal</a>
-                        </div>
-
-                    </div>
-
-                </nav>
-            </div>
-
+           <?php include("includes/navBarContainer.php"); ?>
         </div>
     
         <div id="nowPlayingBarContainer">
