b67be29d84827d9db88956356e85dde7a58e6f91
Abhinav Risal <abhinavrisal99@gmail.com>
2019-07-29 19:07:00 +0545

    added userLogged in variable

--- includes/nowPlayingBar.php
+++ includes/nowPlayingBar.php
@@ -137,20 +137,20 @@ setShuffle = () => {
 }
 
 shuffleArray = (a) => {
-    var j, x, i;
+    var j, x, i
     for (i = a.length - 1; i > 0; i--) {
-        j = Math.floor(Math.random() * (i + 1));
-        x = a[i];
-        a[i] = a[j];
-        a[j] = x;
+        j = Math.floor(Math.random() * (i + 1))
+        x = a[i]
+        a[i] = a[j]
+        a[j] = x
     }
-    return a;
+    return a
 }
 
 setTrack = (trackId, newPlaylist, play) => {
 
     if(newPlaylist != currentPlaylist){
-        currentPlaylist = newPlaylist;
+        currentPlaylist = newPlaylist
         shufflePlaylist = currentPlaylist.slice()
         shuffleArray(shufflePlaylist)
     }
