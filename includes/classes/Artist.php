9eb7605307770d4184a0a3a1c534a0ae5a523790
Abhinav Risal <abhinavrisal99@gmail.com>
2019-07-30 22:28:54 +0545

    Added search functionality for songs and artist

--- includes/classes/Artist.php
+++ includes/classes/Artist.php
@@ -10,6 +10,9 @@ Class Artist {
         $this->id = $id;
     }
 
+    public function getId(){
+        return $this->id;
+    }
     public function getName(){
         $artistsQuery = mysqli_query($this->con, "SELECT name FROM artists where id= '$this->id'");
         $artist = mysqli_fetch_array($artistsQuery);
