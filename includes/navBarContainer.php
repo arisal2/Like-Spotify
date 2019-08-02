7fb5817b1f49ad6e54848cfc0e9829d7a439a72c
Abhinav Risal <abhinavrisal99@gmail.com>
2019-08-02 22:43:36 +0545

    Updated updateDetails

--- includes/navBarContainer.php
+++ includes/navBarContainer.php
@@ -22,7 +22,7 @@
                             <span role="link" tabindex="0" onclick="openPage('yourMusic.php')" class="navItemLink">Your Music</span>
                         </div>
                         <div class="navItem">
-                            <span role="link" tabindex="0" onclick="openPage('settings.php')" class="navItemLink">Abhinav Risal</span>
+                            <span role="link" tabindex="0" onclick="openPage('settings.php')" class="navItemLink"><?php echo $userLoggedIn->getFirstAndLastName(); ?></span>
                         </div>
 
                     </div>
--- updateDetails.php
+++ updateDetails.php
@@ -1,1 +1,23 @@
-USER DETAILS
+<?php 
+
+include("includes/includedFile.php");
+
+?>
+
+<div class="userDetails">
+    <div class="container borderBottom">
+        <h2>EMAIL</h2>
+        <input type="text" class="email" name="email" placeholder="Email address..." value="<?php echo $userLoggedIn->getEmail(); ?>">
+        <span class="message"></span>
+        <button class="button" onclick=""></button>
+    </div>
+
+    <div class="container">
+        <h2>PASSWORD</h2>
+        <input type="password" class="oldPassword" name="oldPassword" placeholder="Current password...">
+        <input type="password" class="newPassword1" name="newPassword1" placeholder="New password...">
+        <input type="password" class="newPassword2" name="newPassword2" placeholder="Re-type password...">
+        <span class="message"></span>
+        <button class="button" onclick=""></button>
+    </div>
+</div>
